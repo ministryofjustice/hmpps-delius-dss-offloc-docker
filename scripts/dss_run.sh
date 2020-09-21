9ff631e644a0d3c6512416b56a7ec78f90eea4c1
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-09-21 10:17:07 +0100

    updated check_log_errors function to allow n row failures before failing job

--- scripts/dss_run.sh
+++ scripts/dss_run.sh
@@ -71,10 +71,48 @@ function fileimporter_config {
 }
 
 function check_log_errors {
-    if [ $(grep FATAL $1 | wc -l) -gt 0 ]; then
-        echo "Fatal errors detected in $1"
-        err_exit $1 2
+    
+    FATALERRORSFILENAME='./fatalerrors.txt'
+    grep FATAL $1 | tr -d '"' > $FATALERRORSFILENAME
+
+    # max number of parse errors to fail the job on
+    PARSEERRORMAXLIMIT=10
+    # if we pass in override as env var PARSEERRORMAXLIMITOVERRIDE update to use this override
+    if [ ! -z $PARSEERRORMAXLIMITOVERRIDE ]; then
+        PARSEERRORMAXLIMIT=$PARSEERRORMAXLIMITOVERRIDE
     fi
+    # track number of FATAL errors in this file not ones we expect (parser)
+    FATALERRORCOUNTNOTAPARSEERROR=0
+    LINEPARSEERRORCOUNT=0
+
+    while read line; do
+        echo "Checking Line: $line"
+        if [ $(echo $line | grep 'An\|exception\|occurred\|when\|parsing\|data\|in\|P-NOMIS\|file\|at\|line\|number' | wc -l) -eq 0 ]; then
+            echo "Fatal errors detected other than line parse error"
+            FATALERRORCOUNTNOTAPARSEERROR=$((FATALERRORCOUNTNOTAPARSEERROR+1))
+        else
+            echo "Line parse error detected, ignore this error unless we get >= $PARSEERRORMAXLIMIT occurrence. "
+            LINEPARSEERRORCOUNT=$((LINEPARSEERRORCOUNT+1))
+        fi
+        # echo "Line check completed."
+    done < "$FATALERRORSFILENAME"
+
+    echo "FATALERRORCOUNTNOTAPARSEERROR: $FATALERRORCOUNTNOTAPARSEERROR"
+    echo "PARSEERRORMAXLIMIT:  $PARSEERRORMAXLIMIT"
+    echo "LINEPARSEERRORCOUNT: $LINEPARSEERRORCOUNT"
+
+    if [ "$FATALERRORCOUNTNOTAPARSEERROR" -gt 0 ]; then
+        echo "Fatal errors detected in $1 that were not parse errors"
+    else
+        echo "No Fatal errors detected in $1 that were not parse errors"
+        if [ "$LINEPARSEERRORCOUNT" -ge "$PARSEERRORMAXLIMIT" ]; then
+            echo "Fatal parse error count of $LINEPARSEERRORCOUNT is >= limit of $PARSEERRORMAXLIMIT in file $1"
+            err_exit $1 2
+        else
+            echo "Fatal parse error count of $LINEPARSEERRORCOUNT is less than limit of $PARSEERRORMAXLIMIT in file $1"
+        fi
+    fi
+
 }
 
 # Only fetch params if not in build environment
