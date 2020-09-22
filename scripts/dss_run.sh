741dd5901c527297345e1454b69e795ced2470f2
Chris Kinsella <chris.kinsella@helkinsoft.com>
2020-09-22 09:59:14 +0100

    updated run script to use correct file path for download

--- scripts/dss_run.sh
+++ scripts/dss_run.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
 
-DSS_ROOT=/dss
-# Use keyword flags in exit codes to allow Cloudwatch Log Metrics to be defined
-SUCCESS_FLAG="DSS_SUCCESS"
-ERROR_FLAG="DSS_ERROR"
-
-if [ -z $DSS_AWSREGION ]; then  
-    DSS_AWSREGION="eu-west-2"
-fi
-
-if [ -z $DSS_PROJECT ]; then
-    DSS_PROJECT="delius"
-fi
 
 function err_exit {
+    echo 'err_exit'
     echo "$ERROR_FLAG Task $1 exited with code $2";
     exit $2;
 }
 
 # Override config parameters if container launced with corresponding ENVVARS (all have prefix DSS_)
 function encryptiontool_config {
+    echo 'encryptiontool_config'
     sed -i "s/___DSSIV___/$1/g" $DSS_ROOT/encryptionutility/resource/encryption.properties
 }
 
 function dsswebserver_config {
+    echo 'dsswebserver_config'
     # DSSWebServer Config
     # Credentials will be pulled from SSM
     if [ !  -z $DSS_DSSWEBSERVERURL ]; then
         # URL requires a diff seperator for sed
-        sed -i "s#url=https:\/\/localhost:8080#url=$DSS_DSSWEBSERVERURL#g" $DSS_ROOT/offloc/DSSWebService.properties.template
+        sed -i "s/url=https:\/\/localhost:8080/url=$DSS_DSSWEBSERVERURL/g" $DSS_ROOT/offloc/DSSWebService.properties.template
     fi
-    # Fetch per env SSM Creds
+    # # Fetch per env SSM Creds
     sed -i "s/username=___CHANGEME___/username=$1/g" $DSS_ROOT/offloc/DSSWebService.properties.template
     sed -i "s/password=___CHANGEME___/password=$2/g" $DSS_ROOT/offloc/DSSWebService.properties.template
 }
 
 function hmpsserver_config {
+    echo 'hmpsserver_config'
     if [ !  -z $DSS_HMPSSERVERURL ]; then
         # URL requires a diff seperator for sed
         sed -i "s#url=https:\/\/localhost:8080#url=$DSS_HMPSSERVERURL#g" $DSS_ROOT/offloc/HMPSServerDetails.properties.template
@@ -46,6 +38,7 @@ function hmpsserver_config {
 }
 
 function filetransfer_config {
+    echo 'filetransfer_config'
     CONF=$DSS_ROOT/filetransfer/resource/FileTransfer.properties
 
     if [ ! -z $DSS_PNOMISFILEEXTENSION ]; then
@@ -61,17 +54,19 @@ function filetransfer_config {
         sed -i "s/test.mode=false/test.mode=$DSS_TESTMODE/g" $CONF
     fi
     if [ ! -z $DSS_TESTFILE ]; then 
-        sed -i "s/test.offloc.file.path/test.mode=$DSS_TESTFILE/g" $CONF
+        # sed -i "s/test.offloc.file.path/test.mode=$DSS_TESTFILE/g" $CONF
+        sed -i "s/test.offloc.file.path=\/dss_artefacts\/test_file.zip/test.offloc.file.path=\/dss_artefacts\/$DSS_TESTFILE/g" $CONF
     fi
     sed -i "s/___DSSIV___/$1/g" $CONF
 }
 
 function fileimporter_config {
+    echo 'fileimporter_config'
     sed -i "s/___DSSIV___/$1/g" $DSS_ROOT/fileimporter/resource/FileImporter.properties
 }
 
 function check_log_errors {
-    
+    echo 'check_log_errors'
     FATALERRORSFILENAME='./fatalerrors.txt'
     grep FATAL $1 | tr -d '"' > $FATALERRORSFILENAME
 
@@ -112,11 +107,26 @@ function check_log_errors {
             echo "Fatal parse error count of $LINEPARSEERRORCOUNT is less than limit of $PARSEERRORMAXLIMIT in file $1"
         fi
     fi
-
 }
 
+# function main {}
+DSS_ROOT=/dss
+# Use keyword flags in exit codes to allow Cloudwatch Log Metrics to be defined
+SUCCESS_FLAG="DSS_SUCCESS"
+ERROR_FLAG="DSS_ERROR"
+
+if [ -z $DSS_AWSREGION ]; then  
+    DSS_AWSREGION="eu-west-2"
+fi
+
+if [ -z $DSS_PROJECT ]; then
+    DSS_PROJECT="delius"
+fi
+
+# env
+
 # Only fetch params if not in build environment
-if [ -z $DSS_BUILDTESTMODE ]; then
+if [ -z $DSS_TESTMODE ]; then
     # Get list of params in this region that match predetermined path
     echo "Fetching DSS credentials from SSM..."
     DSS_PARAMS_JSON=$(aws ssm get-parameters --names "/$DSS_ENVIRONMENT/$DSS_PROJECT/apacheds/apacheds/dss_user" "/$DSS_ENVIRONMENT/$DSS_PROJECT/apacheds/apacheds/dss_user_password" --with-decryption --region $DSS_AWSREGION)
@@ -142,6 +152,7 @@ fi
 
 # Generate random 16byte Initialisation vector
 IV=$(strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 16 | tr -d '\n'; echo)
+echo $IV
 
 # Set config values
 dsswebserver_config $DSS_WEB_USER $DSS_WEB_PASSWORD
@@ -151,10 +162,12 @@ filetransfer_config $IV
 fileimporter_config $IV
 
 # Encrypt sensitive files
+echo 'Encrypt sensitive files'
 cd $DSS_ROOT/encryptionutility
 java -cp ./*:lib/*:encryptionutility.jar:resource uk.co.bconline.ndelius.dss.common.impl.CredentialsGenerator ../offloc/DSSWebService.properties.template ../offloc/DSSWebService.properties ../offloc/DSSWebService.keyfile
 java -cp ./*:lib/*:encryptionutility.jar:resource uk.co.bconline.ndelius.dss.common.impl.CredentialsGenerator ../offloc/HMPSServerDetails.properties.template ../offloc/HMPSServerDetails.properties ../offloc/HMPSServerDetails.keyfile
 
+echo 'generate enc props file'
 if [ ! -f $DSS_ROOT/offloc/DSSWebService.keyfile ] && [ ! -f $DSS_ROOT/offloc/DSSWebService.properties ]; then
     echo "Error - Failed to generate encrypted properties file for DSSWebService"
     err_exit DSSWebServiceEnryption 4
@@ -164,9 +177,9 @@ elif [ ! -f $DSS_ROOT/offloc/HMPSServerDetails.keyfile ] && [ ! $DSS_ROOT/offloc
 fi
 
 # If build flag is passed, then do not proceed with actually running the dss batch task
-if [ "$DSS_BUILDTESTMODE" == "true" ]; then
+if [ "$DSS_TESTMODE" == "true" ]; then
     echo "DSS_SUCCESS Ending run as build flag passed. Exiting..."
-    sleep 30
+    sleep 10
     exit 0
 fi
 
@@ -174,19 +187,31 @@ fi
 cd $DSS_ROOT/filetransfer 
 java -cp filetransfer.jar:resource uk.co.bconline.ndelius.dss.filetransfer.FileTransfer
 FTRESULT=$?
+
+# check filetransfer was successful - there's a file
+OFFLOCFILEPATH=$(grep "^offloc.file.path" /dss_config/FileTransfer.properties | cut -d '=' -f 2)
+echo "Checking file download '$OFFLOCFILEPATH' exists to confirm if downloaded offloc file exists.."
+ls -al $OFFLOCFILEPATH
+if test -f "$OFFLOCFILEPATH"; then
+    echo "$OFFLOCFILEPATH exists."
+else
+    echo "'$OFFLOCFILEPATH' does not exists so there was an issue with file transfer."
+    err_exit FileTransfer 2
+fi
+
 # Wait for FI to finish
 while [ $(ps -o pid,args | grep "fileimporter.jar" | grep -v grep | awk '{print $1}'|wc -l) -gt 0 ] ; do 
     echo "Waiting for FileImporter process to finish"; 
     sleep 10; 
 done
 # FileTransfer logs are output to stdout/stderr, but the child FileImporter logs are only written to file - print it for Cloudwatch
-echo "FileImporter Logs folow:"
+echo "FileImporter Logs follow:"
 cat /dss/fileimporter/fileimporter.log
 
 echo "FT Result == $FTRESULT"
 if [ $FTRESULT -eq 0 ]; then
     echo "Checking logs for errors..."
-    check_log_errors /dss/filetransfer/filetransfer.log
+    # check_log_errors /dss/filetransfer/filetransfer.log
     check_log_errors /dss/fileimporter/fileimporter.log
 else
     err_exit FileTransfer $FTRESULT
